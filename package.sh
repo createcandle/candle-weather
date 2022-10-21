01f423bd14e0d6b5cd1cea9fe8a8164564d89d87
flatsiedatsie <unfold@gmail.com>
2022-10-21 13:13:37 +0200

    Update package.sh

--- package.sh
+++ package.sh
@@ -26,7 +26,7 @@ mkdir -p lib package
 pip3 install -r requirements.txt -t lib --no-binary :all: --prefix ""
 
 # Put package together
-cp -r lib pkg LICENSE manifest.json *.py README.md  css images js views  package/
+cp -r lib pkg LICENSE manifest.json *.py README.md  package/
 find package -type f -name '*.pyc' -delete
 find package -type f -name '._*' -delete
 find package -type d -empty -delete
